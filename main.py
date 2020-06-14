2ba78055816d21851ee0ae30bf03dc12d956ad3b
sniu0320 <sniu0320@icloud.com>
2020-06-14 22:08:03 +0800

    20200614

--- main.py
+++ main.py
@@ -1,17 +1,56 @@
+# -*- coding: UTF-8 -*-
 
+import os
+import sys
 import logging
-from moduls.class_TelentClinet import TelnetClient
+import logging.handlers
 
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(filename)s[%(funcName)s line:%(lineno)d] - %(levelname)s: %(message)s')
 
-host = '172.11.3.100'
-command1 = 'show version'
-command2 = 'show board-info'
-telnet_client = TelnetClient()
-print("we are try to telnet to %s" %(host))
-# login to the host, success:return true, fail: return false
-if telnet_client.login_host(host):
-    telnet_client.input_command('command1')
-    telnet_client.input_command('command2')
-    telnet_client.logout()
+class Menu:
+    '''Display a menu and respond to choices when run'''
+
+    def __init__(self):
+        self.choices = {
+            "1": self.run_one,
+            "6": self.quit,
+        }
+
+    def display_menu(self):
+        print("""
+        Tools Menu(2020):
+            轮询工具：
+
+            日志处理：
+                XX
+            运维工具：
+                XX
+
+            6.Quit
+        """)
+
+    def run(self):
+        '''Display the menu and respond to choices'''
+        try:
+            while True:
+                self.display_menu()
+                choice = input("Enter an option: ")
+                action = self.choices.get(choice)
+                if action:
+                    action()
+                else:
+                    print("{} is not a valid choice.".format(choice))
+        except KeyboardInterrupt:
+            print("\nThank you for using this tool.")
+        finally:
+            pass
+
+    def run_one(self):
+        pass
+
+    def quit(self):
+        print("Thank you for using this tool.")
+        sys.exit(0)
+
+if __name__ == '__main__':
+    #logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s]%(message)s',)
+    Menu().run()
