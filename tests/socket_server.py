9a5820947fce929e93d558cfb10b840708326004
sniu0320 <sniu0320@icloud.com>
2020-07-11 21:09:45 +0800

    20200711

--- tests/socket_server.py
+++ tests/socket_server.py
@@ -1,25 +1,28 @@
 # -*- coding: UTF-8 -*-
 import socket
 import os
+import time
 
 server=socket.socket() #
-server.bind(('localhost',6969))
+server.bind(('localhost',6968))
 server.listen(5) #最大监听的连接数量
 print("开始监听")
 
 while True:
-    con, addr = server.accept()
-    print(con, addr)
-    print('{} clinet in...'.format(addr))
+    conn, addr = server.accept()
+    print("客户端{}接入:".format(addr))
     while True:
-        data=con.recv(1024)
-        print("recv:",data.decode('utf-8'))
+        print("等待命令")
+        data=conn.recv(1024)
         if not data:
-            print("{} client has lost...".format(addr))
+            print("客户端{}已断开".format(addr))
             break
-        # con.send(data.upper())
-        res=os.popen(data.decode('utf-8')).read()
-        # print(type(res))
-        con.sendall(res.encode('utf-8'))
+        print("执行命令:",data.decode())
+        cmd_res=os.popen(data.decode()).read() #接受字符串，返回也是字符串
+        if len(cmd_res)==0: 
+            cmd_res='command not found'  #输入命令行错误时，返回
+        conn.send(str(len(cmd_res.encode())).encode('utf-8')) #先发数据大小给客户端
+        client_ack = conn.recv(1024) #解决粘包问题,增加ack
+        conn.send(cmd_res.encode('utf-8'))
 
 server.close()
