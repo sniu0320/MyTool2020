44a629cf0c44c26767db31aa721c191eaf37ef4e
sniu0320 <sniu0320@icloud.com>
2020-07-17 00:02:09 +0800

    20200716

--- core/modules/iptool.py
+++ core/modules/iptool.py
@@ -2,6 +2,7 @@
 
 import ipaddress
 import logging
+from logtool import log_error
 
 
 class ipTool(object):
@@ -20,17 +21,16 @@ class ipTool(object):
         :return True or False
         """
         try:
-            ipaddress.ip_address(addr)
+            ipaddress.ip_address(addr.split('/')[0])
+            logging.debug("'{}' is a valid address".format(addr))
             return True
-        # except Exception as e:
-        #     print(">>>ERROR: {}".format(e))
-        except Exception:
-            # logging.error("{} \n".format(addr), exc_info=True)
-            logging.error("", exc_info=True)
+        except:
+            logging.debug("'{}' is not a valid address".format(addr))
             return False
 
     @staticmethod
-    def network_hosts(networks):
+    @log_error
+    def network_num_hosts(networks):
         """
         输入要查询的网段，输出主机地址list（去除了基地址和广播地址），支持v6，networks支持字符串和列表输入。
         :param networks list: ['172.11.3.0/24', '172.11.4.0/24']
@@ -39,18 +39,18 @@ class ipTool(object):
         :return host_list
         """
         host_list = []
-        try:
-            if type(networks) is str:
-                networks = networks.split(',')
-            for n in networks:
-                net = ipaddress.ip_network(n.strip())
-                logging.debug('{!r}'.format(net))
-                host_list += net.hosts()  # 去除基地址和广播地址
-        except Exception:
-            logging.error("", exc_info=True)
+        if type(networks) is str:
+            networks = networks.split(',')
+        for n in networks:
+            if ipTool.is_ip_Valid(n) is not True:
+                continue
+            net = ipaddress.ip_network(n.strip())
+            logging.debug('{!r} num hosts: {}'.format(net, net.num_addresses-2))
+            host_list += net.hosts()  # 去除基地址和广播地址
         return host_list
 
     @staticmethod
+    @log_error
     def network_info(networks):
         """
         打印网段相关信息，支持v6，networks支持字符串和列表输入
@@ -61,25 +61,23 @@ class ipTool(object):
         :print version, is_private, broadcast_address, compressed, \
             netmask, hostmask(反掩码), num_addresses, num_hosts
         """
-        try:
-            if type(networks) is str:
-                networks = networks.split(',')
-            for n in networks:
-                net = ipaddress.ip_network(n.strip())
-                print('{!r}'.format(net))
-                print('        version:', net.version)
-                print('     is private:', net.is_private)
-                print('      broadcast:', net.broadcast_address)
-                print('     compressed:', net.compressed)
-                print('   with netmask:', net.with_netmask)
-                print('  with hostmask:', net.with_hostmask)
-                print('  num addresses:', net.num_addresses)
-                print('      num hosts:', net.num_addresses-2)
-                print()
-        except Exception:
-            logging.error("", exc_info=True)
+        if type(networks) is str:
+            networks = networks.split(',')
+        for n in networks:
+            net = ipaddress.ip_network(n.strip())
+            print('{!r}'.format(net))
+            print('        version:', net.version)
+            print('     is private:', net.is_private)
+            print('      broadcast:', net.broadcast_address)
+            print('     compressed:', net.compressed)
+            print('   with netmask:', net.with_netmask)
+            print('  with hostmask:', net.with_hostmask)
+            print('  num addresses:', net.num_addresses)
+            print('      num hosts:', net.num_addresses-2)
+            print()
 
     @staticmethod
+    @log_error
     def interface_info(addresses):
         """
         打印网关相关信息，支持v6，ip_addresses支持字符串和列表输入
@@ -89,29 +87,24 @@ class ipTool(object):
 
         :print network, ip, IP with prefixlen, netmask, hostmask(反掩码)
         """
-        try:
-            if type(addresses) is str:
-                addresses = addresses.split(',')
-            for ip in addresses:
-                iface = ipaddress.ip_interface(ip.strip())
-                print('{!r}'.format(iface))
-                print('            network:', iface.network)
-                print('                 ip:', iface.ip)
-                print('  IP with prefixlen:', iface.with_prefixlen)
-                print('            netmask:', iface.with_netmask)
-                print('           hostmask:', iface.with_hostmask)
-                print()
-        except Exception:
-            logging.error("", exc_info=True)
+        if type(addresses) is str:
+            addresses = addresses.split(',')
+        for ip in addresses:
+            iface = ipaddress.ip_interface(ip.strip())
+            print('{!r}'.format(iface))
+            print('            network:', iface.network)
+            print('                 ip:', iface.ip)
+            print('  IP with prefixlen:', iface.with_prefixlen)
+            print('            netmask:', iface.with_netmask)
+            print('           hostmask:', iface.with_hostmask)
+            print()
 
 
 if __name__ == '__main__':
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format='%(levelname)s: %(message)s')
-    ipTool.network_hosts(['172.11.3.0/24', '172.11.4.0/24'])
-    # ipTool.network_hosts('172.11.5.0/24, 172.11.6./24')
-    ipTool.network_hosts('172.11.7.0/255.255.255.0')
-    # ipTool.is_ip_Valid('172.1.1.')
-    ipTool.network_info('172.11.0.0/16, 10.85.1.0/24')
-    ipTool.interface_info('172.11.6.1/16, 10.85.11.13/27')
+    # ipTool.is_ip_Valid('172.1.1.0/24')
+    # ipTool.is_ip_Valid('172.1.1./24')
+    ipTool.network_num_hosts(['172.11.3.0/24', ' 172.11.4.0/24'])
+    ipTool.network_num_hosts('172.11.5.0/24, 172.11.6./24')
+    # ipTool.network_hosts('172.11.7.0/255.255.255.0')
+    # ipTool.network_info('172.11.0.0/16, 10.85.1.0/24')
+    # ipTool.interface_info('172.11.6./16, 10.85.11.13/27')
---
+++ core/modules/logtool.py
@@ -0,0 +1,109 @@
+# -*- coding: UTF-8 -*-
+
+import os
+import logging
+import logging.config
+from functools import wraps
+
+CONF_LOG = os.path.join(os.path.abspath(os.path.dirname(
+    os.path.dirname(os.path.dirname(__file__)))), "conf/logging_config.ini")
+
+logging.config.fileConfig(CONF_LOG)
+
+# LOG_KEY = 'root'
+
+# https://www.runoob.com/w3cnote/python-func-decorators.html
+
+
+def use_log(func):
+    """
+    函数被调用打印 ”func is running“ （装饰器示例）
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        logging.debug("%s is running" % func.__name__)  # 这记录的是wrapper 而不是func
+        return func(*args, **kwargs)
+    return wrapper
+
+
+def use_log2(level):
+    """
+    函数被调用打印 ”func is running“ （带参数装饰器示例）
+    :param level: warn info
+    """
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if level == "warn":
+                logging.warn("%s is running" % func.__name__)
+            elif level == "info":
+                logging.info("%s is running" % func.__name__)
+            return func(*args)
+        return wrapper
+    return decorator
+
+
+class logit(object):
+    """
+    装饰器类 主要使用call，可以创建子类，添加功能（示例）;这个还没弄明白
+    """
+    def __init__(self):
+        pass
+
+    def __call__(self, func):
+        @wraps(func)
+        def wrapped_function(*args, **kwargs):
+            self.notify()
+            return func(*args, **kwargs)
+        return wrapped_function
+
+    def notify(self):
+        # 随便干点啥
+        print("notify call*****************")
+        pass
+
+
+def log_error(func):
+    """
+    给func 添加 try except,记录error信息
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        # logger = logging.getLogger(LOG_KEY)
+        try:
+            return func(*args, **kwargs)
+        except Exception:
+            logging.error("运行错误：", exc_info=True)
+    return wrapper
+
+
+def log_selflog(cls):
+    """
+    给类添加 self.log
+    """
+    @wraps(cls)
+    def wrapper(*args, **kwargs):
+        # logger = logging.getLogger(LOG_KEY)
+        if not hasattr(cls, 'log'):
+            setattr(cls, 'log', logging)
+        return(cls(*args, **kwargs))
+    return wrapper
+
+
+if __name__ == '__main__':
+    @logit()
+    @log_error
+    def test():
+        a = '1'
+        b = a+1
+        return b
+    test()
+
+    @use_log
+    @log_selflog
+    class Test:
+        def add(self):
+            for _ in range(5):
+                self.log.info('ceshi.')
+    t = Test()
+    t.add()
