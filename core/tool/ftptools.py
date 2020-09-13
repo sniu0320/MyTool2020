9e388ed11d7b59f6b21a1160e0f81a6b446e3d43
sniu0320 <sniu0320@icloud.com>
2020-09-13 23:56:38 +0800

    20200913-2

--- core/tool/ftptools.py
+++ core/tool/ftptools.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
 import sys
@@ -8,13 +7,14 @@ import ftplib
 
 class FtpTools(object):
 
-    def __init__(self, ftpserver, user='autotest',  passwd='autotest', remotedir=None):
+    def __init__(self, ftpserver, user='test',  passwd='test', remotedir=None):
         self.ftpserver = ftpserver
         self.user = user
         self.passwd = passwd
         self.remotedir = remotedir
         self.connection = self.connectFtp()
-        self.localdir = self.getlocaldir()
+        self.localdir = self.getlocaldir()  # 没用
+        self.fcount = self.dcount = 0
 
     def connectFtp(self):
         connection = ftplib.FTP(self.ftpserver)
@@ -66,11 +66,13 @@ class FtpTools(object):
         self.connection.retrbinary('RETR {}'.format(remotename), fp.write, buf_size, callback=download_file_progress_bar)
         fp.close()
 
-    def uploadOne(self, localpath, remotename, buf_size=81920):
+    def uploadOne(self, localname, localpath, remotename=None, buf_size=81920):
         """
         upload one file by FTP in text or binary mode
         remote name need not be same as local name
         """
+        if remotename is None:
+            remotename = localname
         self.filesize = os.path.getsize(localpath)
         self.uploadsize = 0
 
@@ -94,6 +96,30 @@ class FtpTools(object):
         self.connection.storbinary("STOR {}".format(remotename), fp, buf_size, callback=upload_file_progress_bar)
         fp.close()
 
+    def uploadDir(self, localdir='.'):
+        """
+        for each directory in an entire tree
+        upload simple files, recur into subdirectories
+        """
+        localfiles = os.listdir(localdir)
+        for localname in localfiles:
+            localpath = os.path.join(localdir, localname)
+            print('uploading', localpath, 'to', localname)
+            if not os.path.isdir(localpath):
+                self.uploadOne(localname, localpath, localname)
+                self.fcount += 1
+            else:
+                try:
+                    self.connection.mkd(localname)
+                    print('{} directory created'.format(localname))
+                except:
+                    print('{} directory not created'.format(localname))
+                self.connection.cwd(localname)            # change remote dir
+                self.uploadDir(localpath)                  # upload local subdir
+                self.connection.cwd('..')                  # change back up
+                self.dcount += 1
+                print('{} directory exited'.format(localname))
+
     def downloadDir(self):
         """
         download all files from remote site/dir per config
@@ -101,45 +127,28 @@ class FtpTools(object):
         """
         remotefiles = self.connection.nlst()         # nlst is remote listing
         for remotename in remotefiles:
-            if remotename in ('.', '..'): continue
+            if remotename in ('.', '..'):
+                continue
             localpath = os.path.join(self.localdir, remotename)
             print('downloading', remotename, 'to', localpath, 'as', end=' ')
             self.downloadOne(remotename, localpath)
         print('Done:', len(remotefiles), 'files downloaded.')
 
-    def uploadDir(self):
-        """
-        upload all files to remote site/dir per config
-        listdir() strips dir path, any failure ends script
-        """
-        localfiles = os.listdir(self.localdir)       # listdir is local listing
-        for localname in localfiles:
-            localpath = os.path.join(self.localdir, localname)
-            print('uploading', localpath, 'to', localname, 'as', end=' ')
-            self.uploadOne(localname, localpath, localname)
-        print('Done:', len(localfiles), 'files uploaded.')
+    # def uploadDir(self):
+    #     """
+    #     upload all files to remote site/dir per config
+    #     listdir() strips dir path, any failure ends script
+    #     """
+    #     localfiles = os.listdir(self.localdir)       # listdir is local listing
+    #     for localname in localfiles:
+    #         localpath = os.path.join(self.localdir, localname)
+    #         print('uploading', localpath, 'to', localname, 'as', end=' ')
+    #         self.uploadOne(localname, localpath, localname)
+    #     print('Done:', len(localfiles), 'files uploaded.')
 
-    def run(self, cleanTarget=lambda:None, transferAct=lambda:None):
-        """
-        run a complete FTP session
-        default clean and transfer are no-ops
-        don't delete if can't connect to server
-        """
-        self.connectFtp()
-        cleanTarget()
-        transferAct()
+    def close(self):
         self.connection.quit()
 
+
 if __name__ == '__main__':
-    ftp = FtpTools()
-    xfermode = 'download'
-    if len(sys.argv) > 1:
-        xfermode = sys.argv.pop(1)   # get+del 2nd arg
-    if xfermode == 'download':
-        ftp.configTransfer()
-        ftp.run(cleanTarget=ftp.cleanLocals,  transferAct=ftp.downloadDir)
-    elif xfermode == 'upload':
-        ftp.configTransfer(site='learning-python.com', rdir='books', user='lutz')
-        ftp.run(cleanTarget=ftp.cleanRemotes, transferAct=ftp.uploadDir)
-    else:
-        print('Usage: ftptools.py ["download" | "upload"] [localdir]')
+    ftp2 = FtpTools()
